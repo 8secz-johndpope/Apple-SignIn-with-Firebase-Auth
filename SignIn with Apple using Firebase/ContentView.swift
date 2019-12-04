7cc83003a091a5130cbbb1ecfa81dcdc06efb1fe
mohammad.mugish <mohammad.mugish@gmail.com>
2019-12-04 14:41:00 +0530

    Initial Commit

---
+++ SignIn with Apple using Firebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SignIn with Apple using Firebase
+//
+//  Created by mohammad mugish on 04/12/19.
+//  Copyright © 2019 mohammad mugish. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
